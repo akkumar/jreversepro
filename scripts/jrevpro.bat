2338d5d83f6a293e3d9cbd69b12f8ca910005ed2
Karthik Kumar <akk.elance@gmail.com>
2009-12-02 16:58:46 -0800

    Set the right path to include files.

--- scripts/jrevpro.bat
+++ scripts/jrevpro.bat
@@ -2,9 +2,9 @@
 
 IF "%JREVERSE_HOME%" == "" goto envpath
 
-set CP=%JREVERSE_HOME%\lib\jreversepro.jar
+set CP=%JREVERSE_HOME%\jreversepro.jar;%JREVERSE_HOME%\lib\commons-cli-1.2.jar
  
-java -classpath %CP% jreversepro.JCmdMain %1 %2 %3 %4 %5
+java -classpath %CP% net.sf.jrevpro.cmd.CommandMain %1 %2 %3 %4 %5
 goto end
 
 :envpath
